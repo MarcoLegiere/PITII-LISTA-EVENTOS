4bc004cc5e367b78cf893bd0d846c20a711b6191
Marco Legiere <marcoaureliolegiere@live.com>
2022-11-27 18:07:48 -0300

    Erro deploy heroku

--- sitelist/sitelist/settings.py
+++ sitelist/sitelist/settings.py
@@ -4,11 +4,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 # Quick-start development settings - unsuitable for production
